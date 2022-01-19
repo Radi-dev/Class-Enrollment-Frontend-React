45e690d2c55dac48c9ffdac8b981b9d0ae3e9f1b
Radi <evaradi18@gmail.com>
2022-01-20 00:39:00 +0100

    submit button color

--- src/components/enroll.js
+++ src/components/enroll.js
@@ -143,7 +143,7 @@ export default function Enroll({ data, onNewReg = (f) => f, ...props }) {
               />
 
               <div className="flex w-full my-4">
-                <button className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                <button className="py-2 px-4  bg-highl-hov focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
                   Submit
                 </button>
               </div>
